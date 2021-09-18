65b1b1690674b2b3310d80709b2cd629d8c9fdbb
Lui22 <30185757+Lui22@users.noreply.github.com>
2021-09-18 16:40:10 +0500

    Add files via upload

---
+++ Userscript.user.js
@@ -0,0 +1,62 @@
+// ==UserScript==
+// @name         Userscript
+// @namespace    http://tampermonkey.net/
+// @version      0.333
+// @description  try to take over the world!
+// @author       You
+// @require      https://cdn.jsdelivr.net/npm/lodash@4.17.21/lodash.min.js
+// @grant        none
+// @include      http://*/*
+// @include      https://*/*
+// ==/UserScript==
+
+'use strict';
+
+let stri
+document.onkeydown = function(e) {
+    if(e.ctrlKey) {
+        switch (e.keyCode) {
+            case 81:
+                console.log('ща')
+                stri = null
+                camelize(getSelectionText())
+        }
+    }
+}
+
+function getSelectionText() {
+    var text = "";
+    if (window.getSelection) {
+        text = window.getSelection().toString();
+    } else if (document.selection && document.selection.type != "Control") {
+        text = document.selection.createRange().text;
+    }
+    return text;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+function camelize(str) {
+
+
+    var xhr = new XMLHttpRequest();
+
+    var params = 'q=' + encodeURIComponent(str) +
+        '&target=' + encodeURIComponent('en') +
+        '&source=' + encodeURIComponent('ru');
+
+    xhr.open("GET", 'https://api.allorigins.win/get?url=' + encodeURIComponent('https://script.google.com/macros/s/AKfycbzTpY-St_55t4l5wbt7Fg19DMMEId_yMftRIUQ4DyBQbgz6fsII/exec?') + params, true);
+
+    xhr.onload = function(){
+        var jsonResponse = xhr.response;
+        stri = JSON.parse(jsonResponse).contents
+        stri = _.kebabCase(stri)
+        console.log(stri)
+        navigator.clipboard.writeText(stri);
+        return stri;
+    };
+
+    xhr.send();
+}
